8e6aa170d6d80673137b2b0c4de8b1538c279242
tea-jean <tjholt@live.com.au>
2020-03-06 19:11:22 +1100

    added Services scaf

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to "services#index"
+  resources :services
   devise_for :users
+  get "walker/dashboard", to: "walker#dashboard"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
