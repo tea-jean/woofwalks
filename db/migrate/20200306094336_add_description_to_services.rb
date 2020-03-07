cdaab5f3ba883c36ec3bbc0a31f495d11f684752
tea-jean <tjholt@live.com.au>
2020-03-07 12:46:12 +1100

    fix duplicate column names

--- db/migrate/20200306094336_add_description_to_services.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToServices < ActiveRecord::Migration[6.0]
-  def change
-    add_column :services, :description, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_094336) do
+ActiveRecord::Schema.define(version: 2020_03_06_083237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "services", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.text "description"
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
